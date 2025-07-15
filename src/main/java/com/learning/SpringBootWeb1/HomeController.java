e2bdd09df2f44699305b45cee35165d71f3b6ea4
Mohan Lal <mhnuk2007@gmail.com>
2025-07-15 22:11:33 +0500

    Used @ModelAttribute to bind form data to Alien object

--- src/main/java/com/learning/SpringBootWeb1/HomeController.java
+++ src/main/java/com/learning/SpringBootWeb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +18,7 @@ public class HomeController {
     }
 
     @RequestMapping("addAlien")
-    public ModelAndView add(Alien alien, ModelAndView mv) {
-
-        mv.addObject("alien", alien);
-        mv.setViewName("result");
-       return mv;
-
+    public String add(@ModelAttribute Alien alien) {
+        return "result";
     }
 }
